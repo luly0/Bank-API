f52a24a32a8eb2bae6b3e0108db12d13e6bb6afd
Pedro Ferreira <pedrolaminhamc@gmail.com>
2026-04-24 18:10:14 -0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO account (number, customer_name, balance) VALUES ('ACC001', 'João Silva', 1000.00);
+INSERT INTO account (number, customer_name, balance) VALUES ('ACC002', 'Maria Oliveira', 2000.00);
+INSERT INTO account (number, customer_name, balance) VALUES ('ACC003', 'Guilherme Keler', 1500.00);
